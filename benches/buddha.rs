03587b367dd873ef862ae7fec3f11a2601ce79f0
Tamo <tamo@meilisearch.com>
2021-06-17 23:40:36 +0200

    fix the benches

--- benches/buddha.rs
+++ benches/buddha.rs
@@ -1,32 +1,32 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
-use mandelbrust::Mandel;
+use buddhabrust::Buddha;
 
 fn bench_small(c: &mut Criterion) {
-    let mandel = Mandel::new(-1.3, -0.4, 16, 800.0);
+    let buddha = Buddha::new(-1.3, -0.4, 16, 800.0);
     let mut screen = vec![0; 800 * 800];
     c.bench_function("mandelbrot 800 * 800 & 128 iterations", |b| {
         b.iter(|| {
-            mandel.compute(&mut screen, black_box(800), black_box(800));
+            buddha.compute(&mut screen, black_box(800), black_box(800));
         })
     });
 }
 
 fn bench_large(c: &mut Criterion) {
-    let mandel = Mandel::new(-1.3, -0.4, 16, 800.0);
+    let buddha = Buddha::new(-1.3, -0.4, 16, 800.0);
     let mut screen = vec![0; 4000 * 4000];
     c.bench_function("mandelbrot 4000 * 4000 & 128 iterations", |b| {
         b.iter(|| {
-            mandel.compute(&mut screen, black_box(4000), black_box(4000));
+            buddha.compute(&mut screen, black_box(4000), black_box(4000));
         })
     });
 }
 
 fn bench_iter(c: &mut Criterion) {
-    let mandel = Mandel::new(-1.3, -0.4, 1024, 800.0);
+    let buddha = Buddha::new(-1.3, -0.4, 1024, 800.0);
     let mut screen = vec![0; 50 * 50];
     c.bench_function("mandelbrot 50 * 50 & 8096 iterations", |b| {
         b.iter(|| {
-            mandel.compute(&mut screen, black_box(50), black_box(50));
+            buddha.compute(&mut screen, black_box(50), black_box(50));
         })
     });
 }
