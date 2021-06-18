4aba7ab807316b6397978fbc44141549bf7bce2c
Tamo <tamo@meilisearch.com>
2021-06-18 20:44:59 +0200

    add a half_mandel

--- src/buddha.rs
+++ src/buddha.rs
@@ -44,8 +44,8 @@ impl Buddha {
     }
 
     fn bouddha(&self, window: &[AtomicU32], iter: u32, orig_x: usize, orig_y: usize, width: usize, height: usize) {
-        let c_x = orig_x as f64 / self.zoom + self.pos.x as f64;
-        let c_y = orig_y as f64 / self.zoom + self.pos.y as f64;
+        let c_y = orig_x as f64 / self.zoom + self.pos.x as f64;
+        let c_x = orig_y as f64 / self.zoom + self.pos.y as f64;
         let mut z_x = c_x;
         let mut z_y = c_y;
         let mut i = 0;
@@ -59,8 +59,8 @@ impl Buddha {
             z_x = z_x * z_x - z_y * z_y + c_x;
             z_y = 2.0 * z_y * tmp + c_y;
 
-            let y = (z_x * self.zoom).round() as isize + orig_x as isize;
-            let x = (z_y * self.zoom).round() as isize + orig_y as isize;
+            let x = (z_x * self.zoom).round() as isize + orig_x as isize;
+            let y = (z_y * self.zoom).round() as isize + orig_y as isize;
             if (0..width as isize).contains(&x) && (0..height as isize).contains(&y) {
                 window[x as usize + y as usize * width].fetch_add(1, Ordering::Relaxed);
             }
